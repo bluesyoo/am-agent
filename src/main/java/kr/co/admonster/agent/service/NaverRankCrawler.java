76510f83cacb68e9a3da8d27a8d6d47d17a59d7b
blues <blues@bizspring.co.kr>
2025-09-23 17:12:35 +0900

    kafka

--- src/main/java/kr/co/admonster/agent/service/NaverRankCrawler.java
+++ src/main/java/kr/co/admonster/agent/service/NaverRankCrawler.java
@@ -1,8 +1,6 @@
 package kr.co.admonster.agent.service;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
